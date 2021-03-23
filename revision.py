0fd5d0cc4c7d89976be4e1c208a28a6f556f69f7
wojcjas501 <wojcjas501@student.polsl.pl>
2021-03-23 23:57:46 +0100

    New commit

--- revision.py
+++ revision.py
@@ -14,36 +14,38 @@ def if_empty(countries):
 def country_or_capital():
 
     decision = input("What would you like to revise? For capitals press 1, for countries press 2: ")
-    new_countries = database.get_all_countries()
-    random_country = random.choice(new_countries)
-    if decision == "1":
-        value = 'capital'
-        key = 'country'
-    elif decision == "2":
-        value = 'country'
-        key = 'capital'
-    else:
-        print("Unknown choice")
-        value = None
-        key = None
-    guess = input(f"{random_country[key]} - ")
-    while decision == "1" or decision == "2" or guess != "EXIT":
+    while decision == "1" or decision == "2":
+
+        new_countries = database.get_all_countries()
+        random_country = random.choice(new_countries)
+        if decision == "1":
+            value, key = 'capital', 'country'
+        elif decision == "2":
+            value, key = 'country', 'capital'
+        else:
+            break
+        guess = input(f"{random_country[key]} - ")
         if guess == "EXIT":
             break
-        elif guess == random_country[value]:
-            print("You are right!")
-            random_country = random.choice(new_countries)
-            guess = input(f"{random_country[key]} - ")
-        else:
-            print("You are wrong")
-            if decision == "1":
-                print(f"Capital of {random_country['country']} is {random_country['capital']}")
+        while decision == "1" or decision == "2" :
+            if guess == "EXIT":
+                break
+            elif guess == random_country[value]:
+                print("You are right!")
                 random_country = random.choice(new_countries)
                 guess = input(f"{random_country[key]} - ")
-            elif decision == "2":
-                print(f"{random_country['capital']} is capital of {random_country['country']}")
-                random_country = random.choice(new_countries)
-                guess = input(f"{random_country[key]} - ")
+            else:
+                print("You are wrong")
+                if decision == "1":
+                    print(f"Capital of {random_country['country']} is {random_country['capital']}")
+                    random_country = random.choice(new_countries)
+                    guess = input(f"{random_country[key]} - ")
+                elif decision == "2":
+                    print(f"{random_country['capital']} is capital of {random_country['country']}")
+                    random_country = random.choice(new_countries)
+                    guess = input(f"{random_country[key]} - ")
+    else:
+        print("Unknown choice")
 
 
 def revise():
